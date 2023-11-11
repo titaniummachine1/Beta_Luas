eefe1813f1a7d39fb32945e5f9e2735a99c86732
titanium_machine <78664175+titaniummachine1@users.noreply.github.com>
2023-11-11 18:12:37 +0100

    Add files via upload

--- Auto Trickstab.lua
+++ Auto Trickstab.lua
@@ -14,6 +14,12 @@ local function GetHitboxPos(player, hitboxID)
     return hitbox and (hitbox[1] + hitbox[2]) * 0.5 or nil
 end
 
+-- Normalizes a vector to a unit vector
+local function NormalizeVector(vec)
+    local length = math.sqrt(vec.x^2 + vec.y^2 + vec.z^2)
+    return Vector3(vec.x / length, vec.y / length, vec.z / length)
+end
+
 local cachedLocalPlayer
 local cachedPlayers = {}
 local cachedLoadoutSlot2
@@ -45,48 +51,19 @@ local function UpdatePlayersCache()
     end
 end
 
-
 -- Initialize cache
 UpdateLocalPlayerCache()
 UpdatePlayersCache()
-
-local function CanBackstabFromPosition(cmd, viewPos)
-    local weaponReady = cachedLoadoutSlot2 ~= nil
-    if not weaponReady then return false end
-
-    for _, targetPlayer in pairs(cachedPlayers) do
-        if targetPlayer.isAlive and not targetPlayer.isDormant and targetPlayer.teamNumber ~= cachedLocalPlayer:GetTeamNumber() then
-            local distance = math.abs(viewPos.x - targetPlayer.hitboxPos.x) + math.abs(viewPos.y - targetPlayer.hitboxPos.y) + math.abs(viewPos.z - targetPlayer.hitboxPos.z)
-            if distance < 105 then
-                local ang = PositionAngles(viewPos, targetPlayer.hitboxPos)
-                cmd:SetViewAngles(ang:Unpack())
-
-                if cachedLoadoutSlot2:GetPropInt("m_bReadyToBackstab") == 257 then
-                    return true
-                end
-            end
-        end
-    end
-
-    return false
-end
-
-
 
 -- Constants
 local NUM_DIRECTIONS = 8  -- Example: 8 directions (N, NE, E, SE, S, SW, W, NW)
 local MAX_SPEED = 320  -- Maximum speed
 local SIMULATION_TICKS = 24  -- Number of ticks for simulation
+local positions = {}
 
 local FORWARD_COLLISION_ANGLE = 55
 local GROUND_COLLISION_ANGLE_LOW = 45
 local GROUND_COLLISION_ANGLE_HIGH = 55
-
--- Normalizes a vector to a unit vector
-local function NormalizeVector(vec)
-    local length = math.sqrt(vec.x^2 + vec.y^2 + vec.z^2)
-    return Vector3(vec.x / length, vec.y / length, vec.z / length)
-end
 
 -- Helper function for forward collision
 local function handleForwardCollision(vel, wallTrace, vUp)
@@ -126,12 +103,14 @@ local function PredictPlayer(player, simulatedVelocity)
     local vHitbox = { Vector3(-20, -20, 0), Vector3(20, 20, 80) }
     local vStep = Vector3(0, 0, stepSize / 2)
 
+    positions = {}  -- Store positions for each tick
     local lastP = player:GetAbsOrigin()
     local lastV = simulatedVelocity
     local flags = player:GetPropInt("m_fFlags")
     local lastG = (flags & FL_ONGROUND == 1)
 
     for i = 1, SIMULATION_TICKS do
+        
         local pos = lastP + lastV * tick_interval
         local vel = lastV
         local onGround = lastG
@@ -157,53 +136,141 @@ local function PredictPlayer(player, simulatedVelocity)
         end
 
         lastP, lastV, lastG = pos, vel, onGround
+        positions[i] = lastP  -- Store position for this tick
     end
 
-    return lastP
+    return positions
 end
 
--- Simulate walking in multiple directions around a player
-local function SimulateWalkingInDirections(player)
-    local originalPosition = player:GetAbsOrigin()
+-- Constants
+local BACKSTAB_RANGE = 105  -- Hammer units
+local BACKSTAB_ANGLE = 180  -- Degrees in radians for dot product calculation
+
+local function CanBackstabFromPosition(cmd, viewPos, real)
+    local weaponReady = cachedLoadoutSlot2 ~= nil
+    if not weaponReady then return false end
+
+    for _, targetPlayer in pairs(cachedPlayers) do
+        if targetPlayer.isAlive and not targetPlayer.isDormant and targetPlayer.teamNumber ~= cachedLocalPlayer:GetTeamNumber() then
+            local distance = math.abs(viewPos.x - targetPlayer.hitboxPos.x) + 
+                             math.abs(viewPos.y - targetPlayer.hitboxPos.y) + 
+                             math.abs(viewPos.z - targetPlayer.hitboxPos.z)
+            if distance < BACKSTAB_RANGE then
+                local ang = PositionAngles(viewPos, targetPlayer.hitboxPos)
+                cmd:SetViewAngles(ang:Unpack())
+
+                if real then
+                    return cachedLoadoutSlot2:GetPropInt("m_bReadyToBackstab") == 257
+                else
+                    local enemy_back_direction = -NormalizeVector(targetPlayer.viewOffset)
+                    local spy_to_enemy_direction = NormalizeVector(targetPlayer.hitboxPos - viewPos)
+                    local dot_product = spy_to_enemy_direction.x * enemy_back_direction.x +
+                                        spy_to_enemy_direction.y * enemy_back_direction.y +
+                                        spy_to_enemy_direction.z * enemy_back_direction.z
+                    local angle = math.acos(dot_product) * (180 / math.pi)
+                    print(angle)
+                    return angle <= BACKSTAB_ANGLE / 2
+                end
+            end
+        end
+    end
+
+    return false
+end
+
+local function GetBestTarget(me)
+    local players = entities.FindByClass("CTFPlayer")
+    local bestTarget = nil
+    local maxDistance = 160  -- 24 ticks into future at speed of 320 units
+
+    for _, player in pairs(players) do
+        if player ~= nil and player:IsAlive() and not player:IsDormant()
+        and player ~= me and player:GetTeamNumber() ~= me:GetTeamNumber() then
+            local distance = vector.Distance(me:GetAbsOrigin(), player:GetAbsOrigin())
+
+            if distance <= maxDistance then
+                if bestTarget == nil or distance < vector.Distance(me:GetAbsOrigin(), bestTarget:GetAbsOrigin()) then
+                    bestTarget = player
+                end
+            end
+        end
+    end
+
+    return bestTarget
+end
+
+
+local function SimulateWalkingInDirections(player, target)
     local endPositions = {}
+    local playerPos = player:GetAbsOrigin()
+    local targetPos = target:GetAbsOrigin()
+
+    -- Calculate the direction towards the target
+    local targetDirection = NormalizeVector(targetPos - playerPos)
+
+    -- Calculate the starting angle for the target direction
+    local startAngle = math.deg(math.atan(targetDirection.y, targetDirection.x))
 
     for direction = 1, NUM_DIRECTIONS do
-        local angle = (direction - 1) * (360 / NUM_DIRECTIONS)
+        local angle = (startAngle + (direction - 1) * (360 / NUM_DIRECTIONS)) % 360
         local radianAngle = math.rad(angle)
+
         local directionVector = NormalizeVector(Vector3(math.cos(radianAngle), math.sin(radianAngle), 0))
         local simulatedVelocity = directionVector * MAX_SPEED
 
-        -- Use the PredictPlayer function to simulate movement in this direction
-        local endPosition = PredictPlayer(player, simulatedVelocity)
-        endPositions[angle] = endPosition
+        -- Predict player position based on the simulated direction
+        endPositions[angle] = PredictPlayer(player, simulatedVelocity)
     end
 
     return endPositions
 end
 
-local warps = {}
+
+local allWarps = {}
+local endwarps = {}
 
 local function OnCreateMove(cmd)
     UpdateLocalPlayerCache()  -- Update local player data every tick
     UpdatePlayersCache()  -- Update player data every tick
 
+    allWarps = {}
+    endwarps = {}
+    BackstabOportunity = Vector3(0, 0, 0)
 
-    warps = SimulateWalkingInDirections(cachedLocalPlayer)
-    --for i = 1, 1000 do --crash test
-        if CanBackstabFromPosition(cmd, pLocalViewPos) then
-            cmd:SetButtons(cmd.buttons | IN_ATTACK)  -- Perform backstab
+    -- Store all potential positions in allWarps
+    local target = GetBestTarget(cachedLocalPlayer)
+    if not target then return end
+
+    local currentWarps = SimulateWalkingInDirections(cachedLocalPlayer, target)
+
+    table.insert(allWarps, currentWarps)
+
+    -- Storing 24th tick positions in endwarps
+    for angle, positions in pairs(currentWarps) do
+        if positions[24] then
+            endwarps[angle] = positions[24]
         end
-    --end
+    end
+
+        -- check if any of warp positions can stab anyone
+        for angle, point in pairs(endwarps) do
+            if CanBackstabFromPosition(cmd, point + Vector3(0, 0, 75), false) then
+                BackstabOportunity = point
+            end
+        end
+    if CanBackstabFromPosition(cmd, pLocalViewPos, true) then
+        cmd:SetButtons(cmd.buttons | IN_ATTACK)  -- Perform backstab
+    end
 end
+
+
 
 local consolas = draw.CreateFont("Consolas", 17, 500)
 local current_fps = 0
 local function doDraw()
     draw.SetFont(consolas)
     draw.Color(255, 255, 255, 255)
-    local plocal = entities.GetLocalPlayer()
-    local speed = plocal:EstimateAbsVelocity():Length()
-    print(speed)
+
     -- update fps every 100 frames
     if globals.FrameCount() % 100 == 0 then
       current_fps = math.floor(1 / globals.FrameTime())
@@ -211,15 +278,37 @@ local function doDraw()
   
     draw.Text(5, 5, "[lmaobox | fps: " .. current_fps .. "]")
 
-    -- Drawing the threat points on screen
-    for i, point in pairs(warps) do
-        local screenPos = client.WorldToScreen(Vector3(point.x, point.y, point.z))
-        if screenPos then
-            draw.Color(0, 255, 0, 255)  -- Green color for points
-            local x, y = screenPos[1], screenPos[2]
-            draw.FilledRect(x - 2, y - 2, x + 2, y + 2)  -- Draw a small square centered at (x, y)
+    -- Drawing all simulated positions in green
+    for _, warps in ipairs(allWarps) do
+        for angle, positions in pairs(warps) do
+            for _, point in ipairs(positions) do
+                local screenPos = client.WorldToScreen(Vector3(point.x, point.y, point.z))
+                if screenPos then
+                    draw.Color(0, 255, 0, 255)
+                    draw.FilledRect(screenPos[1] - 2, screenPos[2] - 2, screenPos[1] + 2, screenPos[2] + 2)
+                end
+            end
         end
     end
+
+    -- Drawing the 24th tick positions in red
+    for angle, point in pairs(endwarps) do
+        local screenPos = client.WorldToScreen(Vector3(point.x, point.y, point.z))
+        if screenPos then
+            draw.Color(255, 0, 0, 255)
+            draw.FilledRect(screenPos[1] - 5, screenPos[2] - 5, screenPos[1] + 5, screenPos[2] + 5)
+        end
+    end
+
+    -- Drawing backstab Position
+    if BackstabOportunity then
+        local screenPos = client.WorldToScreen(Vector3(BackstabOportunity.x, BackstabOportunity.y, BackstabOportunity.z))
+        if screenPos then
+            draw.Color(255, 255, 0, 255)
+            draw.FilledRect(screenPos[1] - 5, screenPos[2] - 5, screenPos[1] + 5, screenPos[2] + 5)
+        end
+    end
+
 end
 
 callbacks.Unregister("CreateMove", "OnCreateMove123313")
@@ -227,5 +316,3 @@ callbacks.Register("CreateMove", "OnCreateMove12313", OnCreateMove)
 
 callbacks.Unregister("Draw", "AMsadaAT_Draw")                        -- Unregister the "Draw" callback
 callbacks.Register("Draw", "AMsadaAT_Draw", doDraw)                               -- Register the "Draw" callback
-
-
