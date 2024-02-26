12e4b66164da8174d060f5a94daca55b0b2289d0
titanium_machine <78664175+titaniummachine1@users.noreply.github.com>
2024-02-26 01:28:03 +0100

    Add files via upload

--- A_standstillDummy.lua
+++ A_standstillDummy.lua
@@ -31,41 +31,39 @@ local function WalkTo(pCmd, pLocal, pDestination)
     local localPos = pLocal:GetAbsOrigin()
     local distVector = pDestination - localPos
     local dist = distVector:Length()
-
-    -- Determine the speed based on the distance
-    local speed = math.max(MIN_SPEED, math.min(MAX_SPEED, dist))
+    local velocity = pLocal:EstimateAbsVelocity():Length()
 
     -- If distance is greater than 1, proceed with walking
     if dist > 1 then
         local result = ComputeMove(pCmd, localPos, pDestination)
-
-        -- Scale down the movements based on the calculated speed
-        local scaleFactor = speed / MAX_SPEED
-        pCmd:SetForwardMove(result.x * scaleFactor)
-        pCmd:SetSideMove(result.y * scaleFactor)
-    else
-        pCmd:SetForwardMove(0)
-        pCmd:SetSideMove(0)
+        -- If distance is less than 10, scale down the speed further
+        if dist < 10 + velocity then
+            local scaleFactor = dist / 100
+            pCmd:SetForwardMove(result.x * scaleFactor)
+            pCmd:SetSideMove(result.y * scaleFactor)
+        else
+            pCmd:SetForwardMove(result.x)
+            pCmd:SetSideMove(result.y)
+        end
     end
 end
 
 local returnVec = entities.GetLocalPlayer():GetAbsOrigin()
-PosPlaced = true
+local PosPlaced = true
 
 local function OnCreateMove(pCmd)
     local pLocal = entities.GetLocalPlayer()
-    if not pLocal then return end
+    if not pLocal and pLocal:IsAlive() then return end
 
-    if pLocal:IsAlive() then
-        local localPos = pLocal:GetAbsOrigin()
+    if input.IsButtonDown(KEY_LSHIFT) then
+        returnVec = entities.GetLocalPlayer():GetAbsOrigin()
+        PosPlaced = false
+    else
+        PosPlaced = true
+    end
 
-            local distVector = returnVec - localPos
-            local dist = distVector:Length()
-            if dist > 20 then
-                WalkTo(pCmd, pLocal, returnVec)
-            else
-                return
-            end
+    if PosPlaced then
+        WalkTo(pCmd, pLocal, returnVec)
     end
 end
 
